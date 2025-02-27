d6e05d08e718eb8077e40225b9d60a650e1f8e16
alec <alecrondel@hotmail.com>
2025-02-27 18:02:31 -0300

    tailwind 4.0 migration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import { JSX } from "react";
 
 export default function Home(): JSX.Element {
     return (
-      <div className="p-6">
-        <h1 className="text-3xl font-bold">Home Page</h1>
+      <div className="p-6 bg-gray-20">
+        <h1 className="text-3xl font-bold text-red-500">Home Page</h1>
         <p>Welcome to your React app!</p>
       </div>
     );
