5235a0ce1e8c38a751ef3b5d1c3914a935db2d2e
alec <alecrondel@hotmail.com>
2025-03-01 00:58:33 -0300

    criando router links para menu do topo

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { JSX } from "react";
+import { JSX, useState } from "react";
+import { SelectedPage } from "./shared/types";
 
 export default function App(): JSX.Element {
+
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+
   return (
     <Router>
-      <Navbar />
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,65 @@
 import { JSX } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // import { useState } from "react";
 // import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-// import Logo from "@/assets/logo_dataforest.png";
-// import Logo2 from "@/assets/Logo.png";
+import Logo from "../assets/logo_dataforest2.png";
+import NavbarLinks from "./NavbarLinks";
+import { SelectedPage } from "../shared/types";
+
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+}
 
 const flexBetween = "flex justify-between items-center";
 
-export default function Navbar(): JSX.Element {
+export default function Navbar({selectedPage, setSelectedPage}: Props): JSX.Element {
   return (
     <nav>
       <div className={`${flexBetween} bg-blue-500 fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
-            <img src="../assets/logo_dataforest.png" alt="logo"/>
+            {/* left side */}
+            <img src={Logo} alt="logo"/>
+            {/* right side */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <NavbarLinks 
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <NavbarLinks
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <NavbarLinks
+                  page="Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <NavbarLinks
+                  page="Contact us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a member</button>
+              </div>
+            </div>
           </div>
         </div>
-        <ul className="flex space-x-4 text-white">
+        {/* <ul className="flex space-x-4 text-white">
           <li>
             <Link to="/" className="hover:underline">Home</Link>
           </li>
           <li>
             <Link to="/about" className="hover:underline">About</Link>
           </li>
-        </ul>
+        </ul> */}
       </div>
     </nav>
   );
