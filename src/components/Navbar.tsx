5cea5c40c1401efa5a8a4b7455fca3d9b55360dc
alec <alecrondel@hotmail.com>
2025-02-28 23:55:51 -0300

    estilizando navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,30 @@
 import { JSX } from "react";
 import { Link } from "react-router-dom";
+// import { useState } from "react";
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+// import Logo from "@/assets/logo_dataforest.png";
+// import Logo2 from "@/assets/Logo.png";
+
+const flexBetween = "flex justify-between items-center";
 
 export default function Navbar(): JSX.Element {
   return (
-    <nav className="bg-blue-500 p-4">
-      <ul className="flex space-x-4 text-white">
-        <li>
-          <Link to="/" className="hover:underline">Home</Link>
-        </li>
-        <li>
-          <Link to="/about" className="hover:underline">About</Link>
-        </li>
-      </ul>
+    <nav>
+      <div className={`${flexBetween} bg-blue-500 fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img src="../assets/logo_dataforest.png" alt="logo"/>
+          </div>
+        </div>
+        <ul className="flex space-x-4 text-white">
+          <li>
+            <Link to="/" className="hover:underline">Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:underline">About</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
