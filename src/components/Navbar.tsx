0c7da78542e82414565c8d1cfe1f40e58d7c1d90
alec <alecrondel@hotmail.com>
2025-03-06 17:57:01 -0300

    media query e update menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,26 @@
 import { JSX } from "react";
 // import { Link } from "react-router-dom";
-// import { useState } from "react";
-// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 import Logo from "../assets/logo_dataforest2.png";
 import NavbarLinks from "./NavbarLinks";
 import { SelectedPage } from "../shared/types";
+import useMediaQuery from "../hooks/useMediaQuery";
+import ActionButton from "../shared/ActionButton";
+// import { div } from "framer-motion/client";
 
 type Props = {
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 }
 
-const flexBetween = "flex justify-between items-center";
-
 export default function Navbar({selectedPage, setSelectedPage}: Props): JSX.Element {
+
+  const flexBetween = "flex justify-between items-center";
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+
+
   return (
     <nav>
       <div className={`${flexBetween} bg-blue-500 fixed top-0 z-30 w-full py-6`}>
@@ -22,44 +29,47 @@ export default function Navbar({selectedPage, setSelectedPage}: Props): JSX.Elem
             {/* left side */}
             <img src={Logo} alt="logo"/>
             {/* right side */}
-            <div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <NavbarLinks 
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <NavbarLinks
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <NavbarLinks
-                  page="Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <NavbarLinks
-                  page="Contact us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            { isAboveMediumScreens ? (
+              // DESKTOP SCREENS
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <NavbarLinks 
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <NavbarLinks
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <NavbarLinks
+                    page="Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <NavbarLinks
+                    page="Contact us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>Become a member</ActionButton>
+                </div>
               </div>
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <button>Become a member</button>
-              </div>
-            </div>
+            ) : (
+              // MOBILE SCREENS
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="text-white h-6 w-6" />
+              </button>
+            )}
           </div>
         </div>
-        {/* <ul className="flex space-x-4 text-white">
-          <li>
-            <Link to="/" className="hover:underline">Home</Link>
-          </li>
-          <li>
-            <Link to="/about" className="hover:underline">About</Link>
-          </li>
-        </ul> */}
       </div>
     </nav>
   );
