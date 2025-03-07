1b9b825e1a5f7c4187e7b58724abd51234961dee
Alec <alecrondel@hotmail.com>
2025-03-06 22:26:19 -0300

    ajuste pagina inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,19 @@ export default function Home(): JSX.Element {
             <br />
             <p className="text-xs">SAÚDE FLORESTAL | MINIMIZAÇÃO DE RISCOS | PLANTIO INTELIGENTE</p>
             <br />
-            <button
-              className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
-            >
-              Comece Aqui!
-            </button>
+            <div className="flex gap-2">
+              <button
+                className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
+              >
+                Comece Aqui!
+              </button>
+              <span className="pt-4">ou</span>
+            </div>
+            <br />
+            <a href="" className="pt-10 underline decoration-solid">Me leve até o dashboard.</a>
           </div>
           <div className="w-2xl">
-            <img src={Plant} alt="arrow" />
+            <img src={Plant} alt="Reflorestamento" />
           </div>
         </div>
       </div>
