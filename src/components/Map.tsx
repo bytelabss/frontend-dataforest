52a65a0aa3ca75347f63375a541af4c406920e75
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2025-03-27 20:47:55 -0300

    feat (LABS-18): Popup com as informações das áreas ao clicar sobre

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,14 @@
 import { JSX, useEffect, useState } from "react";
-import { MapContainer, TileLayer, Polygon } from 'react-leaflet';
+import { MapContainer, TileLayer, Polygon, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { LatLng } from "leaflet";
+import { LatLng, LatLngBounds } from "leaflet";
 import './styles.css';
 
-
-
 export default function Map(): JSX.Element {
-
   
   interface Geometry {
     type: string;
-    coordinates: number[][][]; // O formato é um array de arrays de arrays de coordenadas (Polygon)
+    coordinates: number[][][]; 
   }
   
   interface ReforestedArea {
@@ -24,12 +21,13 @@ export default function Map(): JSX.Element {
     created_at: string;
     updated_at: string;
   }
+
   const [areas, setAreas] = useState<ReforestedArea[]>([]);
 
   useEffect(() => {
     const getAreas = async () => {
       const areasData = await fetchReforestedAreas();
-      console.log("areasData : " +areasData)
+      console.log("areasData : " + areasData);
       if (areasData.length > 0) {
         setAreas(areasData);  
         console.log('Estado atualizado com as áreas:', areasData);  
@@ -42,7 +40,6 @@ export default function Map(): JSX.Element {
     
   }, []);
   
-  // Função para buscar os dados das áreas reflorestadas
   const fetchReforestedAreas = async (): Promise<ReforestedArea[]> => {
     try {
       const response = await fetch('http://127.0.0.1:5000/reforested_areas');
@@ -76,12 +73,23 @@ export default function Map(): JSX.Element {
       />
       
       {areas.map((area) => {
-        const coordinates = area.geom.coordinates[0].map(coord => new LatLng( coord[0],coord[1],)); // [latitude, longitude]
+        const coordinates = area.geom.coordinates[0].map(coord => new LatLng(coord[0], coord[1])); // [latitude, longitude]
         
-        console.log("Coordenadas para poligono: " + coordinates)
+        console.log("Coordenadas para poligono: " + coordinates);
+
+        const polygonBounds = new LatLngBounds(coordinates);
+        const center = polygonBounds.getCenter();
 
         return (
-          <Polygon key={area.id} positions={coordinates} pathOptions={blackOptions} />
+          <Polygon key={area.id} positions={coordinates} pathOptions={blackOptions}>
+            <Popup position={center}>
+              <div>
+                <h3>{area.name}</h3>
+                <p><strong>Descrição:</strong> {area.description}</p>
+                <p><strong>Área:</strong> {area.area_in_m2} m²</p>
+              </div>
+            </Popup>
+          </Polygon>
         );
       })}
     </MapContainer>
